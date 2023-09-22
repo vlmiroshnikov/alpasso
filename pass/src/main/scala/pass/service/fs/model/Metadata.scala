25698b3e3631c8aea399b4ba69c1bb3d7ae8caa9
Vyacheslav Miroshnikov <vlmiroshnikov@gmail.com>
2023-09-22 23:03:44 +0300

    refactoring

--- pass/src/main/scala/pass/service/fs/model/Metadata.scala
+++ pass/src/main/scala/pass/service/fs/model/Metadata.scala
@@ -5,18 +5,18 @@ import cats.syntax.all.*
 import io.circe.*
 import io.circe.syntax.*
 
-opaque type Metadata = JsonObject
+opaque type Metadata = Map[String, String]
 
 object Metadata:
 
   def fromString(raw: String): Either[Exception, Metadata] =
-    if raw.nonEmpty then parser.parse(raw).flatMap(_.as[JsonObject])
+    if raw.nonEmpty then parser.parse(raw).flatMap(_.as[Map[String, String]])
     else Metadata.empty.asRight
 
-  extension (m: Metadata) def rawString: String = Printer.spaces2.print(m.toJson)
+  extension (m: Metadata) def rawString: String = Printer.spaces2.print(m.asJson)
 
   given Show[Metadata] =
-    Show.show(v => v.toList.map((a, b) => s"${a}:${b.toString}").mkString(", "))
-  def of(kv: Map[String, String]): Metadata = JsonObject.fromMap(kv.view.mapValues(_.asJson).toMap)
+    Show.show(v => v.toList.map((a, b) => s"$a=$b").mkString(","))
+  def of(kv: Map[String, String]): Metadata = kv
 
-  val empty: Metadata = JsonObject.empty
+  val empty: Metadata = Map.empty
